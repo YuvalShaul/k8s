55f7695b8bed5a74920fe42371533d18bd01b190
Yuval Shaul <yuval.shaul@gmail.com>
2026-02-19 12:23:50 +0200

    updating tf file to add access to admins

--- eks/terraform/eks_terraform_config.tf
+++ eks/terraform/eks_terraform_config.tf
@@ -46,6 +46,24 @@ module "eks" {
     }
   }
 
+resource "aws_eks_access_entry" "admins" {
+  for_each      = toset(var.admin_users)
+  cluster_name  = aws_eks_cluster.this.name # Dynamic reference to your cluster
+  principal_arn = each.value
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admins" {
+  for_each      = toset(var.admin_users)
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = each.value
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
   # Single node group matching your setup
   eks_managed_node_groups = {
     default = {
--- eks/terraform/vars.tf
+++ eks/terraform/vars.tf
@@ -11,3 +11,9 @@ variable "cluster_name" {
   type        = string
   default     = "learn-eks"
 }
+
+variable "admin_users" {
+  type        = list(string)
+  description = "List of IAM user ARNs to grant admin access to the EKS cluster"
+  default     = []
+}
